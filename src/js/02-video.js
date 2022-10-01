abb7ab7297f2f841c77c65b8dbb71097a63a9427
lia2307 <juliatymchuk23@gmail.com>
2022-10-02 00:26:17 +0300

    done task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { save, load } from './storage';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const onTimeupdate = function (data) {
+  save(CURRENT_TIME, data);
+};
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+const saveData = load(CURRENT_TIME);
+
+if (saveData) {
+  player
+    .setCurrentTime(saveData.seconds)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
