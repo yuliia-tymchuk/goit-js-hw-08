fe2edfd1c7c198822510c3bea283fb4e09440252
lia2307 <juliatymchuk23@gmail.com>
2022-10-02 01:42:22 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+import { save, load, remove } from './storage';
 
+const formRef = document.querySelector('.feedback-form');
+const LOCALE_STORAGE_KEY = 'feedback-form-state';
+
+initPage();
+
+function onFormInput(event) {
+  const { name, value } = event.target;
+
+  let savedData = load(LOCALE_STORAGE_KEY);
+  savedData = savedData ? savedData : {};
+  savedData[name] = value;
+  save(LOCALE_STORAGE_KEY, savedData);
+}
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log({ email: email.value, message: message.value });
+
+  event.currentTarget.reset();
+  remove(LOCALE_STORAGE_KEY);
+}
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function initPage() {
+  const savedData = load(LOCALE_STORAGE_KEY);
+
+  if (!savedData) {
+    return;
+  }
+
+  Object.entries(savedData).forEach(([name, value]) => {
+    formRef.elemets[name].value = value;
+  });
+}
